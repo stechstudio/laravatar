7ed3173cb9a496cdce722fda97debb3856028bf5
DP <dp@reproconnect.com>
2022-07-27 14:08:13 -0400

    put an arbitrary image on the stack

--- resources/views/components/avatar-stack.blade.php
+++ resources/views/components/avatar-stack.blade.php
@@ -5,12 +5,13 @@
     'avatarClass' => '',
     'size' => 100,
     'zIndexStart' => 1,
+    'image' => null,
 ])
 
 <div {{ $attributes->merge(['class' => 'laravatar laravatar-stack', 'style' => 'position: relative;']) }}>
     <x-laravatar::initials
         :class="$avatarClass"
-        style="width: {{ $size }}px; height: {{ $size }}px; position: absolute; z-index: {{ $zIndexStart }};"
+        style="width: {{ $size }}px; height: {{ $size }}px; position: absolute; z-index: {{ $zIndexStart++ }};"
         alt=""
         :first="$first"
         :last="$last"
@@ -19,8 +20,17 @@
 
     <x-laravatar::gravatar
         :class="$avatarClass"
-        style="width: {{ $size }}px; height: {{ $size }}px; position: absolute; z-index: {{ $zIndexStart + 1 }};"
+        style="width: {{ $size }}px; height: {{ $size }}px; position: absolute; z-index: {{ $zIndexStart++ }};"
         alt=""
         :email="$email"
     />
+
+    @if($image)
+        <img
+            class="{{ $avatarClass }} laravatar-image"
+            src="{{ $image }}"
+            style="width: {{ $size }}px; height: {{ $size }}px; position: absolute; z-index: {{ $zIndexStart++ }};"
+            alt=""
+        />
+    @endif
 </div>
